ad6ae3880942b6118fd99491234e1a22d065fd60
Mukesh2080 <mukeshsagar919@gmail.com>
2026-07-19 23:06:05 +0530

    -> version upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.village.generalstore"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
